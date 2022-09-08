1d1445388b80c6a73cd7d753c5633441bab19365
John Diginee <johndbizz@gmail.com>
2022-09-08 05:52:13 -0700

    Updated 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - this program prints "Programming is like building a multilingual puzzle
+ * main - Entry point 
  *
- * return: 0 
+ * Description: this program prints 'Programming is like building a multilingual puzzle'
+ *
+ * return: Always 0 (Success)
  */
 
 int main(void)
