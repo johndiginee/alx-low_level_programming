bea707714a9b2db053da799c9760697629359e27
John Diginee <johndbizz@gmail.com>
2022-09-09 05:17:55 -0700

    Tasks  6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit nums of base 10
+ *
+ * Return : Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num =48; num < 58; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
