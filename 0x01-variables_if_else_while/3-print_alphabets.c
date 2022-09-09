d37780131eaf07b3bc26e072dcbe267a80ba1064
John Diginee <johndbizz@gmail.com>
2022-09-09 02:24:35 -0700

    Tasks  3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase and in uppercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		putchar(ch);
+	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++0)
+		putchar(alphabet);
+	putchar(\n);
+	return (0);
+}
