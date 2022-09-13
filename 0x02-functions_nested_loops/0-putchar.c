2d3c833619d42f1dd7061fedaab7db76eac40d9f
John Diginee <johndbizz@gmail.com>
2022-09-13 04:48:45 -0700

    Corrected an error on  Tasks 0. putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "main.h"
+i#include "main.h"
 
 /**
 * main - prints _putchar, followed by a new line
@@ -7,7 +7,14 @@
 */
 int main(void)
 {
-	_putchar('_putchar');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 
 	return (0);
 }
