65ea5b60b940828e4a42758cce178909b863e7d4
John Diginee <johndbizz@gmail.com>
2022-09-19 06:37:30 -0700

    Tasks  6. Half the lies they tell about me arent true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other char of a string
+ * starting with the first char, followed by a new line
+ * @str: string to print the chars from
+ */
+void puts2(char *str)
+{
+	int length, i;
+
+	length = 0;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	for (i = ; i < length; i += 2)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
