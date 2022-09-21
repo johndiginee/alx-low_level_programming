57c386f857d653a7f21555f65bc4595bfa92371f
John Diginee <johndbizz@gmail.com>
2022-09-21 10:07:47 -0700

    Fixed a bug in 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,10 +10,11 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+	int i;
+	int j;
 
 	for (i = 0; dest[i] != '\0'; i++)
-		;
+
 	for (j = 0; src[j] != '\0'; j++, i++)
 		dest[i] = src[j];
 
