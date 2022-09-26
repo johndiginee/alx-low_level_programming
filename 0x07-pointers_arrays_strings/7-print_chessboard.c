02119ffa90a6fe26ca029500e82972d0d34640be
John Diginee <johndbizz@gmail.com>
2022-09-26 05:30:04 -0700

    Fixed a bug in 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_chessboard - function that prints the chessboard
+ * print_chessboard - prints an 7x8 chessboard
  * @a: 2d char array representing board
  *
  * Return: void
@@ -10,7 +10,7 @@ void print_chessboard(char (*a)[8])
 {
 	int i, j;
 
-	for (i = 0; i < 8; j++)
+	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
 		{
