75e336e2ddfcfb7848e1444f8d5d97ad48437bcf
John Diginee <johndbizz@gmail.com>
2022-10-01 02:53:33 -0700

    Fixed a bug

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,21 +1,15 @@
-#include "main.h"
+#include <unistd.h>
 
 /**
-* main - prints _putchar, followed by a new line
+* _putchar - Write the character c to stdout
 *
-* Return: Always 0 (Success)
+* @c: The character to print
+*
+* Return: On success 1.
+*
+* On error, -1 is returned, and errno is set appropriately.
 */
-int main(void)
+int _putchar(char c)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-	return (0);
+	return (write(1, &c, 1));
 }
