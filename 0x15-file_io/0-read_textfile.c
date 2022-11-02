781a081e029d05498f198bd7a475230abed9ec18
John Diginee <johndbizz@gmail.com>
2022-11-01 22:37:55 -0700

    Fixed a bug in 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,7 +15,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (!filename || !letters)
 		return (0);
-	fd = open(filename, 0_RDONLY);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 	bytes = read(fd, &buf[0], letters);
