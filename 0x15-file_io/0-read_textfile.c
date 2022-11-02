fbb64d0a9c08a4f5fc6335c51f95c708c64d8fde
John Diginee <johndbizz@gmail.com>
2022-11-01 23:24:40 -0700

    Fixed bugs in 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,16 +10,27 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t bytes;
-	char buf[READ_BUF_SIZE * 8];
+	ssize_t nrd, nwr;
+	char *buf;
 
-	if (!filename || !letters)
+	if (!filename)
 		return (0);
+
 	fd = open(filename, O_RDONLY);
+	
 	if (fd == -1)
 		return (0);
-	bytes = read(fd, &buf[0], letters);
-	bytes = write(STDOUT_FILENO, $buf[0], bytes);
+	
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
 	close(fd);
-	return (bytes);
+
+	free(buf);
+
+	return (nwr);
 }
